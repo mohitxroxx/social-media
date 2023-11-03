e057f0c49d4da08a4afb1f381c4805ba35e7bea7
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-03 11:58:20 +0530

    first_userProfile_added

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -1,1 +1,41 @@
 //creating data of user
+
+ export const user ={
+    _id: "64df3c064180b81adfe41d4b",
+    firstName: "Code",
+    lastName: "Wave",
+    email: "sachendra@gmail.com",
+
+    friends: [
+        {
+            _id: "64df3aec4180b81adfe41d32",
+      firstName: "John",
+      lastName: "Bruce",
+      email: "john@gmail.com",
+      friends: ["64df3c064180b81adfe41d4b", "64df39704180b81adfe41d0b"],
+      views: [],
+      verified: true,
+      createdAt: "2023-08-18T09:33:32.519Z",
+      updatedAt: "2023-08-18T09:49:19.475Z",
+      __v: 2,
+      profileUrl:
+        "https://res.cloudinary.com/djs3wu5bg/image/upload/v1683874470/cld-sample.jpg",
+    },  
+    {
+        _id: "64df39704180b81adfe41d0b",
+        firstName: "James",
+        lastName: "Jackson",
+        email: "james@gmail.com",
+        friends: ["64df3c064180b81adfe41d4b", "64df3aec4180b81adfe41d32"],
+        views: [
+          "64df39704180b81adfe41d0b",
+          "64df39704180b81adfe41d0b",
+          "64df39704180b81adfe41d0b",
+          "64df39704180b81adfe41d0b",
+          "64df39704180b81adfe41d0b",
+          "64df39704180b81adfe41d0b",
+        ],    
+
+
+    ]
+}
