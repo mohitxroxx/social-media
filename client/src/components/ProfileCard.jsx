d0f260a070778d5ea75aa1c421f563fd1a7fd489
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-04 00:12:49 +0530

    profile-versio-done

--- client/src/components/ProfileCard.jsx
+++ client/src/components/ProfileCard.jsx
@@ -3,11 +3,15 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { Link } from 'react-router-dom';
 import { NoProfile } from '../assets';
+import { UpdateProfile } from '../redux/userSlice';
+import {LiaEditSolid} from 'react-icons/lia'
+import {BsPersonFillAdd} from 'react-icons/bs'
 
-const ProfileCard = (user) => {
+
+const ProfileCard = ({user}) => {
  const{user: data, edit } = useSelector((state) => state.user);
  const dispatch = useDispatch();
- 
+
     return (
     <div>
       <div className='w-full bg-primary flex flex-col items-centre shadow-sm rounded-xl
@@ -19,11 +23,36 @@ const ProfileCard = (user) => {
  <img src={user?.profileUrl ?? NoProfile} alt={user?.email}
  className='w-14 h-14 object-cover rounded-full'></img>
   <div className='flex flex-col justify-center'>
-   <p className='text-lg font-medium '></p>
+   <p className='text-lg font-medium text-ascent-1'>
+
+  {user?.firstName} {user?.lastName}
+
+   </p> 
+   <span className='text-ascent-2'>{user?.profession ?? "No Profession"}</span>
 
   </div>
 
 </Link>
+
+   <div className=''>
+   {/* agar profile khud ki hai to edit kar paaye brna nhi */}
+         {user?._id === data?._id ? (   <LiaEditSolid
+            size={22}
+            className='text-blue cursor-pointer'
+             onClick={() => dispatch(UpdateProfile(true))}
+           />): (
+       <button className='bg-[#0444a430] text-sm text-white p-1 rounded'
+       onClick={() => {}}
+       >
+      <BsPersonFillAdd  size={20} className='text-[#0f52b6]'/>
+
+
+       </button>
+
+
+           )}
+
+ </div>
 
 </div>
       </div>
