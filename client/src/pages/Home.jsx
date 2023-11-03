5f61fc4796bfe0c458533154f467500c85afe95b
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-03 15:52:49 +0530

    NavBar-done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,10 @@ const Home = () => {
     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg
      h-screen overflow-hidden'>
       <TopBar />
-      
+      <div className='w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full '>
+
+
+      </div>
       
       </div>
   )
