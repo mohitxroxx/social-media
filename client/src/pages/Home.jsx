cf9dcc817b0ad497425116d3da1c12cca86887f7
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-03 16:17:51 +0530

    working on content section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg
      h-screen overflow-hidden'>
       <TopBar />
+      {/* Adding content section */}
       <div className='w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full '>
 
 
