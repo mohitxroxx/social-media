818f48aa71797d7a627652779972486c51bd6102
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-03 12:07:50 +0530

    added_firends_data

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -56,6 +56,71 @@
         __v: 1,
       },
     ],
+    views: [
+        "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    "64df39704180b81adfe41d0b",
+    ],
+    verified: true,
+    createdAt: "2023-08-18T09:38:14.179Z",
+    updatedAt: "2023-08-21T06:46:18.258Z",
+    profileUrl:
+      "https://res.cloudinary.com/djs3wu5bg/image/upload/v1683874454/samples/people/boy-snow-hoodie.jpg",
+    token: "hZWFmZmU3NmMiLCJpYXQiOjE2OTIwMzY5",
+  };
+
+
+  export const friends = [
+    {
+      _id: "64df3aec4180b81adfe41d32",
+      firstName: "John",
+      lastName: "Bruce",
+      email: "john@gmail.com",
+      profileUrl:
+        "https://res.cloudinary.com/djs3wu5bg/image/upload/v1683874470/cld-sample.jpg",
+    },
+    {
+      _id: "64df39704180b81adfe41d0b",
+      firstName: "James",
+      lastName: "Jackson",
+      email: "james@gmail.com",
+      location: "Mumbai, India",
+      profession: "Full-Stack Developer",
+    },
+    {
+      _id: "64df424b4a4c0d47b5369f65",
+      firstName: "User",
+      lastName: "One",
+      email: "user!@gmail.com",
+    },
+  ];
+
+
 
     ]
 }
