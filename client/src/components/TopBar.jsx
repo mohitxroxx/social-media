bfd392369b7f7ae9af84779e5dcd7075109d9e6a
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-03 15:17:41 +0530

    importdone

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
 import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector'
+import { TbSocial } from "react-icons/tb";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import TextInput from "./TextInput";
+import CustomButton from "./CustomButton";
+import { useForm } from "react-hook-form";
+import { BsMoon, BsSunFill } from "react-icons/bs";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { SetTheme } from "../redux/theme";
+import { Logout } from "../redux/userSlice";
+
+
+
 const TopBar = () => {
   const {theme} = useSelector(state=>state.theme)
   const {user} = useSelector(state=>state.user)
   const dispatch = useDispatch();
+
+
+  
     return (
     <div className='topbar w-full flex items-center justify-between py-3 md:py-6 px-4 big-primary'>
       
@@ -34,7 +49,22 @@ const TopBar = () => {
       </form>
 
 {/* icons ko laga rha hu moon and sun bale */}
+<div className='flex gap-4 items-center text-ascent-1 text-md md:text-xl'>
+        <button onClick={() => handleTheme()}>
+          {theme ? <BsMoon /> : <BsSunFill />}
+        </button>
+        <div className='hidden lg:flex'>
+          <IoMdNotificationsOutline />
+        </div>
 
+        <div>
+          <CustomButton
+            onClick={() => dispatch(Logout())}
+            title='Log Out'
+            containerStyles='text-sm text-ascent-1 px-4 md:px-6 py-1 md:py-2 border border-[#666] rounded-full'
+          />
+        </div>
+      </div>
 
     </div>
   )
