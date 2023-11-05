7c807a90016d1bab3eec3e1fef2e195094996d05
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-05 23:40:38 +0530

    edit-profile-structure

--- client/src/components/EditProfile.jsx
+++ client/src/components/EditProfile.jsx
@@ -25,9 +25,21 @@ const EditProfile = () => {
       };
     
   return (
-    <div>
+    <>
+    <div
+    className='fixed z-30 inset-0 overflow-y-auto'>
+        <div className='flex items-center justify-center min-h-screen
+        pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+ <div className='fixed inset-0 transition-opacity'>
+            <div className='absolute inset-0 bg-[#000] opacity-70'>
+          </div>
+          </div>
+
+
+        </div>
       
     </div>
+    </>
   )
 }
 
