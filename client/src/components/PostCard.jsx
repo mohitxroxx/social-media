5febf6611c091caad3df2d7e44fabf432d26b396
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-05 23:21:04 +0530

    added-reply-function

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -11,6 +11,55 @@ import TextInput from "./TextInput";
 import { postComments } from "../assets/data";
 import { useForm } from "react-hook-form";
 
+
+    {/* reply krne ke liye*/}
+    const ReplyCard =({reply ,user ,handleLike}) => {
+        return (
+            <div className='w-full py-3'>
+            <div className='flex gap-3 items-center mb-1'>
+
+                {/* image/profile ke liye */}
+             <Link to={"/profile/" + reply?.userId?._id}>
+             <img src={reply?.userId?.profileUrl ?? NoProfile} alt={reply?.userId?.firstName}
+                  className='w-10 h-10 rounded-full object-cover'
+                />
+              </Link>
+      
+              <div>
+                <Link to={"/profile/" + reply?.userId?._id}>
+                  <p className='font-medium text-base text-ascent-1'>
+                    {reply?.userId?.firstName} {reply?.userId?.lastName}
+                  </p>
+                </Link>
+                <span className='text-ascent-2 text-sm'>
+                  {moment(reply?.createdAt).fromNow()}
+                </span>
+              </div>
+            </div>
+      
+            <div className='ml-12'>
+              <p className='text-ascent-2 '>{reply?.comment}</p>
+              <div className='mt-2 flex gap-6'>
+                <p
+                  className='flex gap-2 items-center text-base text-ascent-2 cursor-pointer'
+                  onClick={handleLike}
+                >
+                  {reply?.likes?.includes(user?._id) ? (
+                    <BiSolidLike size={20} color='blue' />
+                  ) : (
+                    <BiLike size={20} />
+                  )}
+                  {reply?.likes?.length} Likes
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+        )
+    }
+
+
      {/* yeh comment ke liye form*/}
 const CommentForm = ({user ,id , replyAt ,getComments}) =>
 {
@@ -93,6 +142,10 @@ const getComments =async () => {
     setComments(postComments);
     setLoading(false);
 };
+   const handleLike =async () =>
+   {
+
+   };
 
   return (
 
@@ -283,6 +336,15 @@ const getComments =async () => {
                     </p>
                   )}
                 
+                {showReply === comment?.replies?._id &&
+                    comment?.replies?.map((reply) => (
+                      <ReplyCard  reply={reply} user={user} key={reply?._id} 
+                      
+                      handleLike={() => handleLike(
+                            "/posts/like-comment/" +
+                              comment?._id +"/" + reply?._id )  }  />
+                    ))}
+
                  </div>
 
                      </div>
