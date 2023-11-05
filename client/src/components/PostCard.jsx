ffe71c1f4f8bb0bfaafd8efafb942b2572727573
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-05 17:53:47 +0530

    added_description

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { NoProfile } from '../assets';
-
-
+import moment from 'moment';
+import { BiSolidLike ,BiLike } from 'react-icons/bi';
 
 
 const PostCard = ({post ,user ,deletePost ,likePost}) => {
@@ -15,6 +15,7 @@ const PostCard = ({post ,user ,deletePost ,likePost}) => {
  const[replyComments ,setReplyComments] = useState(0);
  const[showComments ,setShowComments] = useState(0);
    
+
 
   return (
     <div className='mb-2 bg-primary p-4 rounded-xl '>
@@ -31,18 +32,33 @@ const PostCard = ({post ,user ,deletePost ,likePost}) => {
 <div className='w-full flex justify-between'>
 
 <div className=''>
-       <Link></Link>
-    
+<Link to={"/profile/" + post?.userId?._id}>
+            <p className='font-medium text-base text-ascent-1'>
+              {post?.userId?.firstName} {post?.userId?.lastName}
+            </p>
+          </Link>
+          <span className='text-ascent-2 text-sm'>
+            {post?.userId?.location}
+          </span>
 </div>
+  
+<span className='text-ascent-2 text-sm'>
+            {moment(post?.createdAt).fromNow()}
+          </span>
+</div>
+ </div>
 
+ <div className='ml-12'>
+        <p className='text-ascent-2 '>
+            
+            
+            {showAll === post?._id ? post?.description : post?.description.slice(0,300)}</p>
+              { post?.description?.length>301 && (
+                showAll === post?._id ? <span>Show Less</span> :(<span>Show More</span>
+              ))}     
+      </div>
 
 </div>
-
-
-
-        </div>
-
-    </div>
   )
 }
 
