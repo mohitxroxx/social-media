c1c4c403e1a6c9f19535fd2bf6e19bb178743c4a
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-05 18:43:24 +0530

    added-comments by form

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -6,6 +6,74 @@ import moment from 'moment';
 import { BiSolidLike ,BiLike , BiComment } from 'react-icons/bi';
 import {MdOutlineDeleteOutline} from 'react-icons/md'
 
+const CommentForm = ({user ,id , replyAt ,getComments})=>
+{
+    const[loading ,setLoading] = useState(false);
+    const [errMsg, setErrMsg] = useState("");
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+      } = useForm({
+        mode: "onChange",
+      });
+      const onSubmit = async (data) => {};
+
+    return (
+        <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='w-full border-b border-[#66666645]'
+      >
+        <div className='w-full flex items-center gap-2 py-4'>
+          <img
+            src={user?.profileUrl ?? NoProfile}
+            alt='User Image'
+            className='w-10 h-10 rounded-full object-cover'
+          />
+  
+          <TextInput
+            name='comment'
+            styles='w-full rounded-full py-3'
+            placeholder={replyAt ? `Reply @${replyAt}` : "Comment this post"}
+            register={register("comment", {
+              required: "Comment can not be empty",
+            })}
+            error={errors.comment ? errors.comment.message : ""}
+          />
+        </div>
+        {errMsg?.message && (
+          <span
+            role='alert'
+            className={`text-sm ${
+              errMsg?.status === "failed"
+                ? "text-[#f64949fe]"
+                : "text-[#2ba150fe]"
+            } mt-0.5`}
+          >
+            {errMsg?.message}
+          </span>
+        )}
+  
+        <div className='flex items-end justify-end pb-2'>
+          {loading ? (
+            <Loading />
+          ) : (
+            <CustomButton
+              title='Submit'
+              type='submit'
+              containerStyles='bg-[#0444a4] text-white py-1 px-3 rounded-full font-semibold text-sm'
+            />
+          )}
+        </div>
+      </form>
+
+
+    )
+}
+
+
+
 const PostCard = ({post ,user ,deletePost ,likePost}) => {
 
  const[showAll ,setShowAll] = useState(0);
@@ -22,6 +90,7 @@ const getComments =async () => {
 };
 
   return (
+    
     <div className='mb-2 bg-primary p-4 rounded-xl '>
       
     <div className='flex gap-3 items-center mb-2'>
@@ -188,8 +257,6 @@ const getComments =async () => {
                   )}
 
              </div>
-
-
 </div>
   )
 }
