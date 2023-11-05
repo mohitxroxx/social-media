4b2749b5d5182472788046c210e041c2993ca875
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-05 15:49:48 +0530

    added_friendRequestOPtion

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,13 @@ import { useSelector } from 'react-redux/es/hooks/useSelector'
 import TopBar from '../components/TopBar'
 import ProfileCard from '../components/ProfileCard'
 import { FriendsCard } from '../components'
+import { friends } from '../assets/data'
+import { useState } from 'react'
+
 const Home = () => {
   const {user} =useSelector(state=>state.user)
-  
+  const [friendRequest ,setFriendRequest] =useState(friends);
+  const [suggestedFriends , setSuggestedFriends] = useState(friends);
   
   
   return (
@@ -27,7 +31,67 @@ const Home = () => {
 
           </div>
             
+      {/* right side ka content */}
+      <div className='hidden w-1/4 h-full lg:flex flex-col gap-8 overflow-y-auto' >
+           {/* friend request */}
+              <div className='w-full bg-primary shadow-sm rounded-lg px-6 py-5'>
+        <div className='flex itesm-center justif-between text-xl text-ascent-1 pb-2 border-b border-[#66666645]'>
+           <span>Friend Request</span>
+           <span>{friendRequest?.length}</span>
 
+        </div>
+
+              <div className='w-full flex flex-col gap-4 pt-4'>
+              
+              {friendRequest?.map(({ _id, requestFrom: from }) => (
+                  <div key={_id} className='flex items-center justify-between'>
+                    <Link
+                      to={"/profile/" + from._id}
+                      className='w-full flex gap-4 items-center cursor-pointer'
+                    >
+                      <img
+                        src={from?.profileUrl ?? NoProfile}
+                        alt={from?.firstName}
+                        className='w-10 h-10 object-cover rounded-full'
+                      />
+                      <div className='flex-1'>
+                        <p className='text-base font-medium text-ascent-1'>
+                          {from?.firstName} {from?.lastName}
+                        </p>
+                        <span className='text-sm text-ascent-2'>
+                          {from?.profession ?? "No Profession"}
+                        </span>
+                      </div>
+                    </Link>
+
+                    <div className='flex gap-1'>
+                      <CustomButton
+                        title='Accept'
+                        containerStyles='bg-[#0444a4] text-xs text-white px-1.5 py-1 rounded-full'
+                      />
+                      <CustomButton
+                        title='Deny'
+                        containerStyles='border border-[#666] text-xs text-ascent-1 px-1.5 py-1 rounded-full'
+                      />
+                    </div>
+                  </div>
+                ))}
+
+
+                </div>  
+
+
+              </div>
+
+            {/* suggestion */}
+             <div className='w-full bg-primary shadow-sm rounded-lg px-5 py-5'>
+
+
+
+
+
+             </div>
+      </div>
       </div>
       
       </div>
