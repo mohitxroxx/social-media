73b6301a582f401bcd3fe9c15635edf6d60dde69
Sachendra79 <sachendragangwar120@gmail.com>
2023-11-05 15:52:20 +0530

    added-center_content

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,20 @@ const Home = () => {
   const {user} =useSelector(state=>state.user)
   const [friendRequest ,setFriendRequest] =useState(friends);
   const [suggestedFriends , setSuggestedFriends] = useState(friends);
-  
+  const [errMsg, setErrMsg] = useState("");
+  const [file, setFile] = useState(null);
+  const [posting, setPosting] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handlePostSubmit = async (data) => {};
+
+
   
   return (
     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg
@@ -28,6 +41,126 @@ const Home = () => {
       </div>
        {/* center ke liye */}
           <div className='flex-1 h-full bg-primary px-4 flex flex-col gap-6 overflow-y-auto'>
+
+          <form
+              onSubmit={handleSubmit(handlePostSubmit)}
+              className='bg-primary px-4 rounded-lg'
+            >
+              <div className='w-full flex items-center gap-2 py-4 border-b border-[#66666645]'>
+                <img
+                  src={user?.profileUrl ?? NoProfile}
+                  alt='User Image'
+                  className='w-14 h-14 rounded-full object-cover'
+                />
+                <TextInput
+                  styles='w-full rounded-full py-5'
+                  placeholder="What's on your mind...."
+                  name='description'
+                  register={register("description", {
+                    required: "Write something about post",
+                  })}
+                  error={errors.description ? errors.description.message : ""}
+                />
+              </div>
+              {errMsg?.message && (
+                <span
+                  role='alert'
+                  className={`text-sm ${
+                    errMsg?.status === "failed"
+                      ? "text-[#f64949fe]"
+                      : "text-[#2ba150fe]"
+                  } mt-0.5`}
+                >
+                  {errMsg?.message}
+                </span>
+              )}
+
+              <div className='flex items-center justify-between py-4'>
+                <label
+                  htmlFor='imgUpload'
+                  className='flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer'
+                >
+                  <input
+                    type='file'
+                    onChange={(e) => setFile(e.target.files[0])}
+                    className='hidden'
+                    id='imgUpload'
+                    data-max-size='5120'
+                    accept='.jpg, .png, .jpeg'
+                  />
+                  <BiImages />
+                  <span>Image</span>
+                </label>
+
+                <label
+                  className='flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer'
+                  htmlFor='videoUpload'
+                >
+                  <input
+                    type='file'
+                    data-max-size='5120'
+                    onChange={(e) => setFile(e.target.files[0])}
+                    className='hidden'
+                    id='videoUpload'
+                    accept='.mp4, .wav'
+                  />
+                  <BiSolidVideo />
+                  <span>Video</span>
+                </label>
+
+                <label
+                  className='flex items-center gap-1 text-base text-ascent-2 hover:text-ascent-1 cursor-pointer'
+                  htmlFor='vgifUpload'
+                >
+                  <input
+                    type='file'
+                    data-max-size='5120'
+                    onChange={(e) => setFile(e.target.files[0])}
+                    className='hidden'
+                    id='vgifUpload'
+                    accept='.gif'
+                  />
+                  <BsFiletypeGif />
+                  <span>Gif</span>
+                </label>
+
+                <div>
+                  {posting ? (
+                    <Loading />
+                  ) : (
+                    <CustomButton
+                      type='submit'
+                      title='Post'
+                      containerStyles='bg-[#0444a4] text-white py-1 px-6 rounded-full font-semibold text-sm'
+                    />
+                  )}
+                </div>
+              </div>
+            </form>
+
+            {loading ? (
+              <Loading />
+            ) : posts?.length > 0 ? (
+              posts?.map((post) => (
+                <PostCard
+                  key={post?._id}
+                  post={post}
+                  user={user}
+                  deletePost={() => {}}
+                  likePost={() => {}}
+                />
+              ))
+            ) : (
+              <div className='flex w-full h-full items-center justify-center'>
+                <p className='text-lg text-ascent-2'>No Post Available</p>
+              </div>
+            )}
+
+
+
+
+
+
 
           </div>
             
@@ -86,7 +219,46 @@ const Home = () => {
             {/* suggestion */}
              <div className='w-full bg-primary shadow-sm rounded-lg px-5 py-5'>
 
+             <div className='flex items-center justify-between text-lg text-ascent-1 border-b border-[#66666645]'>
+                <span>Friend Suggestion</span>
+              </div>
+              <div className='w-full flex flex-col gap-4 pt-4'>
+                {suggestedFriends?.map((friend) => (
+                  <div
+                    className='flex items-center justify-between'
+                    key={friend._id}
+                  >
+                    <Link
+                      to={"/profile/" + friend?._id}
+                      key={friend?._id}
+                      className='w-full flex gap-4 items-center cursor-pointer'
+                    >
+                      <img
+                        src={friend?.profileUrl ?? NoProfile}
+                        alt={friend?.firstName}
+                        className='w-10 h-10 object-cover rounded-full'
+                      />
+                      <div className='flex-1 '>
+                        <p className='text-base font-medium text-ascent-1'>
+                          {friend?.firstName} {friend?.lastName}
+                        </p>
+                        <span className='text-sm text-ascent-2'>
+                          {friend?.profession ?? "No Profession"}
+                        </span>
+                      </div>
+                    </Link>
 
+                    <div className='flex gap-1'>
+                      <button
+                        className='bg-[#0444a430] text-sm text-white p-1 rounded'
+                        onClick={() => {}}
+                      >
+                        <BsPersonFillAdd size={20} className='text-[#0f52b6]' />
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
 
 
 
